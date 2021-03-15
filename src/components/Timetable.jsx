7a2f4f0d566e99276c599e911fdaf5a58da06dd2
MartinGabco <gabcomartin@gmail.com>
2021-03-15 08:16:46 +0100

    First refactoring in order to prepare conditions for proper filtering courses on website - deleting child component Courses.jsx.

--- src/components/Timetable.jsx
+++ src/components/Timetable.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+
+//Server connection
 import axios from 'axios';
 import config from '../config.json';
 
+//Help database connection
+import { getType_1 } from '../database/types.js';
+import { getType_2 } from '../database/types.js';
+
 //Components
-import Courses from './Courses.jsx';
 
 class Timetable extends Component {
     state = { 
-        courses: []
+        courses: [],
+        type_1: [],
+        type_2: []
      }
 
     componentDidMount() {
@@ -15,15 +22,44 @@ class Timetable extends Component {
         .then(res => {
         const courses = res.data;
         this.setState({ courses });
-        })
+        });
+
+        this.setState({ type_1: getType_1()})
+        this.setState({ type_2: getType_2()})
     }
 
-    render() { 
-        const { courses } = this.state;
+    render() {
+        const { courses, type_1, type_2 } = this.state;
 
         return ( 
-            <Courses courses={courses}/>
-        );
+            <div class="tabbable"> 
+                <ul class="nav nav-tabs">
+                    <li class="active"><a href="#tab1" data-toggle="tab">Compulsory courses</a></li>
+                    <li><a href="#tab2" data-toggle="tab">Elective courses</a></li>
+                </ul>
+
+                <div class="tab-content">
+                    <div class="tab-pane active" id="tab1">
+                        {type_1.map(type_1 => (
+                            <button 
+                                key={type_1.id_1}
+                            >
+                                {type_1.title_1}
+                            </button>
+                        ))}                        
+                    </div>
+                    <div class="tab-pane" id="tab2">
+                        {type_2.map(type_2 => (
+                            <button 
+                                key={type_2.id_2}
+                            >
+                                {type_2.title_2}
+                            </button>
+                        ))} 
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
  
---
+++ src/database/types.js
@@ -0,0 +1,15 @@
+const type_1 = [
+    {"id_1": "123", "title_1":"Compulsory courses"}
+]
+
+const type_2 = [
+    {"id_2": "456", "title_2":"Elective courses"}
+]
+
+export function getType_1() {
+    return type_1
+}
+
+export function getType_2() {
+    return type_2
+}
